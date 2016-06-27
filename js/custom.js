07747d35044a6d7894c5058d4a4ef4d7d7428dd0
yonikan <yonikan@gmail.com>
2016-06-27 19:59:14 +0300

    - added sections - removed some sections - replaced animations in diffrent laibry

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@
 
 
 		// Background Strech -----------------------------------------------
-		$(".banner-image").backstretch('images/banner.jpg');
+		$(".banner-image").backstretch('images/backs/banner.jpg');
 
 
 		// Fixed header -----------------------------------------------
@@ -78,27 +78,6 @@
 			});
 		};
 
-
-		// Isotope filters -----------------------------------------------
-		if ($('.isotope-container').length>0) {
-			$(window).load(function() {
-				$('.isotope-container').fadeIn();
-				var $container = $('.isotope-container').isotope({
-					itemSelector: '.isotope-item',
-					layoutMode: 'masonry',
-					transitionDuration: '0.6s',
-					filter: "*"
-				});
-				// filter items on button click
-				$('.filters').on( 'click', 'ul.nav li a', function() {
-					var filterValue = $(this).attr('data-filter');
-					$(".filters").find("li.active").removeClass("active");
-					$(this).parent().addClass("active");
-					$container.isotope({ filter: filterValue });
-					return false;
-				});
-			});
-		};
 
 
 		//Modal -----------------------------------------------
